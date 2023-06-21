25d252df4f0c4b4247b6cf2c9f8500394891d77e
Alexander Kaufmann <akaufmann038@gmail.com>
2023-06-21 10:44:55 -0400

    scraper consistently working

--- pythonApi/api.py
+++ pythonApi/api.py
@@ -1,10 +1,23 @@
 from flask import Flask, request
+from scraper import LinkedIn_Scraper
 
 app = Flask(__name__)
+
+l_scraper = LinkedIn_Scraper()
+
+@app.get("/")
+def create_driver():
+    l_scraper.create_driver()
+
+    return "Done"
 
 @app.post("/generate-message")
 def generate_message():
     data = request.get_json()
+    user_id = data["user_id"]
+        
+    scraped_data = l_scraper.scrape_profile(user_id)
+    print(scraped_data)
 
     return data
 
@@ -12,3 +25,6 @@ if __name__ == '__main__':
     app.run() 
 
 # flask --app api run --debug
+
+# either asked to login or security check
+# https://www.linkedin.com/authwall
--- pythonApi/scraper.py
+++ pythonApi/scraper.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 class LinkedIn_Scraper:
     def __init__(self):
@@ -12,31 +14,44 @@ class LinkedIn_Scraper:
     # create the selenium headless driver
     def create_driver(self):
         chrome_options = ChromeOptions()
-        chrome_options.add_argument("--headless=new")
+        #chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--incognito")
         driver = webdriver.Chrome(options=chrome_options)
 
         self.driver = driver
 
-    def webpage_ready(driver):
-        res1 = driver.find_element(By.CLASS_NAME,"core-section-container__content")
-        res2 = driver.find_element(By.CLASS_NAME,"top-card-layout__title")
-        res3 = driver.find_element(By.CLASS_NAME, "top-card-layout__headline")
-        res4 = driver.find_element(By.CLASS_NAME, "top-card__subline-item")
-        res5 = driver.find_element(By.CLASS_NAME, "profile")
-
-        return res1 and res2 and res3 and res4 and res5
+    def wait_until(self, driver, class_name):
+        # if browser hits authwall, we want to quit driver and open up new one
+        if "https://www.linkedin.com/authwall" in driver.current_url:
+            return True
+        
+        return driver.find_element(By.CLASS_NAME, class_name)
 
     # given a user_id, scrapes that user's linkedin profile
     # user_id has to be the id in the linkedin url
     def scrape_profile(self, user_id):
-        url = "https://www.linkedin.com/in/{}".format(user_id)
+        url = "https://www.linkedin.com/in/{}/".format(user_id)
         self.driver.get(url)
 
         # wait for webpage
-        objects = WebDriverWait(self.driver, timeout=5).until(self.webpage_ready)
+        wait = WebDriverWait(self.driver, timeout=10)
+        wait.until(lambda driver: self.wait_until(driver, "core-section-container__content"))
+        wait.until(lambda driver: self.wait_until(driver, "top-card-layout__title"))
+        wait.until(lambda driver: self.wait_until(driver, "top-card__subline-item"))
+        wait.until(lambda driver: self.wait_until(driver, "profile"))
 
+        if "https://www.linkedin.com/authwall" in self.driver.current_url:
+            # quit current driver, create new driver, navigate to new url
+            self.driver.quit()
+            self.create_driver()
+            return self.scrape_profile(user_id)
+
+        #object = WebDriverWait(self.driver, timeout=10).until(EC.presence_of_element_located((By.XPATH, "//meta[@name='description']")))
+        #time.sleep(3)
+
+        print("url: " + self.driver.current_url)
         src = self.driver.page_source
-        soup = BeautifulSoup(src)
+        soup = BeautifulSoup(src, features="html.parser")
 
         data = self.get_data(soup)
 
@@ -45,9 +60,14 @@ class LinkedIn_Scraper:
     def get_data(self, soup):
         data = {}
     
-        data["full_name"] = soup.find_all(class_="top-card-layout__title")[0].text.strip()
+        full_name_c = soup.find_all(class_="top-card-layout__title")
+        data["full_name"] = full_name_c[0].text.strip()
         
-        data["bio"] = soup.find_all(class_="top-card-layout__headline")[0].text.strip()
+        bio_c = soup.find_all(class_="top-card-layout__headline")
+        if len(bio_c) == 0:
+            data["bio"] = None
+        else:
+            data["bio"] = bio_c[0].text.strip()
         
         data["location"] = soup.find_all('div', {'class': 'top-card__subline-item'})[0].text
 
